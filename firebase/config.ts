09215226923259b6fc8a7242e1d2ccdac5a95cb7
zepriquito7k <wesleyjessen62@gmail.com>
2025-12-05 14:25:05 +0000

    Estrutura auth

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFunctions } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,9 +15,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// EXPORTA O AUTH
-export const auth = getAuth(app);
-export const functions = getFunctions(app);
-
-export default app;
--- firebase/validate.ts
+++
@@ -1,4 +0,0 @@
-export function isValidEmail(email: string): boolean {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
